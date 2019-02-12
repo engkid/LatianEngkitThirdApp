5d3f59a7de7da04fd5708049cd1a474db12adbc1
Engkit Satia Riswara <engkitsatia.riswara@dana.id>
2019-02-12 13:01:44 +0700

    Third App first commit

---
+++ LatianEngkitThirdApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LatianEngkitThirdApp
+//
+//  Created by Engkit Satia Riswara on 12/02/19.
+//  Copyright © 2019 Engkit Satia Riswara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
